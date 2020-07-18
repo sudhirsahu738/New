3036351ab70b41859830810476faeced6998a089
dedshit <62318734+dedshit@users.noreply.github.com>
2020-07-18 14:11:14 +0000

    Shrinked #2

--- shows.py
+++ shows.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup as b4
 
 ### Its simple thing shows that zee5 didn't care about their data theft😬😬 ###
 
+
 class dl:
 
 	def __init__(self, url, tk):
@@ -16,30 +17,24 @@ class dl:
 	def generate(self):
 		r = requests.get(self.url)
 		rr = r.content
-
 		if r.status_code == 200:
 			pass
-
 			try:
 				a = b4(rr, 'html5lib')
-				b = a.find_all("script")[9].prettify()
-				c = json.loads(b[39:-11])["EpisodeDetail"]["episodeData"]['hls'][0].replace('drm', 'hls')
+				b = json.loads(a.find_all("script")[9].prettify()[39:-11])["EpisodeDetail"]["episodeData"]['hls'][0].replace('drm', 'hls')
 				xm = requests.get(self.tk)
 				gj = json.loads(xm.content.decode('utf-8'))['video_token']
 
-				if "index.m3u8" in c:
-					pr = ("https://zee5vod.akamaized.net" + c + gj)
-
-					if "index.m3u8" in pr: ### For my convenience i used 576p . If u want to view  in different quality change pixel####
+				if "index.m3u8" in b:
+					pr = ("https://zee5vod.akamaized.net" + b + gj)
+					if "index.m3u8" in pr: ### For my convinenint i used 576p . If u want to view  in different quality change pixel####
 						cr = pr.replace('index.m3u8', 'index576p/576p.m3u8')
 						print (cr)
-
-				elif "master.m3u8" in c:
-					print (c + gj)
-
+				elif "master.m3u8" in b:
+					print (b + gj)
 			except KeyboardInterrupt:
 				print ("quit")
 
-az = input('\n Enter  tvshow link : ')
+az = input('\n Enter  TvShow link : ') 
 zx = dl(az, "https://useraction.zee5.com/token/")
 zx.generate()
