0830fb42fb20da6e1ed631d7334412a60328349e
dedshit <62318734+dedshit@users.noreply.github.com>
2020-07-19 22:41:01 +0530

    #5

--- shows.py
+++ shows.py
@@ -8,17 +8,17 @@ class dl:
 		self.tk = tk
 	def generate(self):
 		r = requests.get(self.url)
-		rr = r.content
 		if r.status_code == 200:
 			try:
-				a = b4(rr, 'html5lib')
+				a = b4(r.content, 'html5lib')
 				b = a.find_all("script")[9].prettify()
 				c = json.loads(b[39:-11])["EpisodeDetail"]["episodeData"]['hls'][0].replace('drm', 'hls')
 				xm = requests.get(self.tk)
 				gj = json.loads(xm.content.decode('utf-8'))['video_token']
 				if "index.m3u8" in c:
 					pr = ("https://zee5vod.akamaized.net" + c + gj)    
-					if "index.m3u8" in pr: ### For my convenience i used 576p . If u want to view  in different quality change pixel####
+					if "index.m3u8" in pr: 
+### For my convenience i used 576p . If u want to view  in different quality change pixel####
 						cr = pr.replace('index.m3u8', 'index576p/576p.m3u8')
 						print ('\n\n',cr)
 				elif "master.m3u8" in c:
