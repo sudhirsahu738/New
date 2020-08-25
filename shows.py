a17e5eeead0e1778aa728d1f396e0f6d77d42849
dedshit <62318734+dedshit@users.noreply.github.com>
2020-08-25 20:54:41 +0530

    Latest cli support

--- shows.py
+++ shows.py
@@ -1,87 +1,100 @@
-#!/usr/bin/python3
-import requests
-import json
 import re
-from bs4 import BeautifulSoup as b4
-class dl:
-        def __init__(self, url, tk):
-                self.url = url
-                self.tk = tk
-        def generate(self):
-                r = requests.get(self.url)
-                if r.status_code == 200:
-                        try:
-                                a = b4(r.content, 'html5lib')
-                                b = a.find_all("script")[9].prettify()
-                                c = json.loads(b[39:-11])["EpisodeDetail"]["episodeData"]['hls'][0].replace('drm', 'hls')
-                                xm = requests.get(self.tk)
-                                gj = json.loads(xm.content.decode('utf-8'))['video_token']
-                                if "index.m3u8" in c:
-                                    print ("\n Generating link.....")
-                                    pr = ("https://zee5vod.akamaized.net" + c + gj)
-                                    g6 = requests.get(pr)
-                                    c2 = g6.content.decode()
-                                    print ("""
-                                            AVAILABLE QUALITIES:
-                                                1️⃣ ~ 96p
-                                                2️⃣ ~ 144p
-                                                3️⃣ ~ 240P
-                                                4️⃣ ~ 360P
-                                                5️⃣ ~ 480P
-                                                6️⃣ ~ 576P
-                                                7️⃣ ~ 720P
-                                                8️⃣ ~ 1080P
-                                        """)
-                                    ZC = int(input('\n Enter choice : '))
-                                    if ZC == 1:
-                                        G = re.findall('index.*',c2)[7]
-                                        print ('\n',pr.replace('index.m3u8', G))
-                                    elif ZC == 2:
-                                        V = re.findall('index.*',c2)[0]
-                                        print ('\n',pr.replace('index.m3u8', V))
-                                    elif ZC == 3:
-                                        Z = re.findall('index.*',c2)[1]
-                                        print ('\n',pr.replace('index.m3u8', Z))
-                                    elif ZC == 4:
-                                        X = re.findall('index.*',c2)[2]
-                                        print ('\n',pr.replace('index.m3u8',X))
-                                    elif ZC == 5:
-                                        N = re.findall('index.*',c2)[3]
-                                        print ('\n',pr.replace('index.m3u8', N))
-                                    elif ZC == 6:
-                                        B = re.findall('index.*',c2)[4]
-                                        print ('\n',pr.replace('index.m3u8', B))
-                                    elif ZC == 7:
-                                        L = re.findall('index.*',c2)[5]
-                                        print ('\n',pr.replace('index.m3u8',L))
-                                    elif ZC == 8:
-                                        K = re.findall('index.*',c2)[6]
-                                        print ('\n',pr.replace('index.m3u8',K))
-                                elif "master.m3u8" in c:
-                                    d5 = requests.get(c + gj)
-                                    do = d5.content.decode('utf-8')
-                                    print ("""
-                                        LIST OF QUALITIES AVAILABLE:
-                                            1️⃣ ~ 144p
-                                            2️⃣ ~ 240p
-                                            3️⃣ ~ 360p
-                                            4️⃣ ~ 480p
-                                    """)
-                                    sg = int(input('\n Enter Quality :'))
-                                    if sg == 1:
-                                        a5 = re.findall('index.*',do)[0]
-                                        print ('\n\n',c[:-11] + a5)
-                                    elif sg == 2:
-                                        x4 = re.findall('index.*',do)[1]
-                                        print ('\n\n',c[:-11] + x4)
-                                    elif sg == 3:
-                                        c8 = re.findall('index.*',do)[2]
-                                        print ('\n\n',c[:-11] + c8)
-                                    elif sg == 4:
-                                        bh = re.findall('index.*',do)[3]
-                                        print ('\n\n',c[:-11] + bh)
-                        except KeyboardInterrupt:
-                                print ("quit")
-az = input('\n Enter TvShow link : ')
-zx = dl(az, "https://useraction.zee5.com/token/")
-zx.generate()
+import sys
+import argparse
+import requests
+import bs4
+
+def usage():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--premiere", help="To download upcoming shows from Zee5", action="store_true")
+    parser.add_argument("-s", "--previous", help="To downloads old shows", action="store_true")
+    parser.add_argument("-q", help="Quality option", action="store_true")
+    args, unknown = parser.parse_known_args()
+def shows():
+    try:
+        usage()
+        if sys.argv[1] == "-p" or "--premiere":
+            gt_link = sys.argv[2]
+            get_request = requests.get(gt_link)
+            response_parse = bs4.BeautifulSoup(get_request.content, 'html5lib')
+            clt_response = response_parse.find_all("script")[9].prettify()
+            url = []
+            for i in clt_response.split(','):
+                if "hls" in i:
+                    url.append(i)
+            re = ((url[-1])[11:-1].replace("drm", "hls"))
+            rt = (str("https://{}".format("zee5vod.akamaized.net") + re))
+            return rt
+        elif sys.argv[1] == "-s" or "--previous":
+            get_link = sys.argv[2]
+            request = requests.get(get_link)
+            parse = bs4.BeautifulSoup(request.content, 'html5lib')
+            parz_resp = parse.find_all("script")[9].prettify()
+            urls = []
+            for _ in parz_resp.split(','):
+                if "hls" in _:
+                    urls.append(_)
+            re1 = (urls[-1][11:-1])
+            re2 = (re1 + tk_m3u)
+            return re2
+        else:
+            pass
+    except IndexError:
+        print("Usage : python3 shows.py -h, --help")
+        
+def main():
+    try:
+        usage()
+        if "index.m3u8" in shows():
+            gt_pix = sys.argv[4]
+            X3 = (shows()[:-10])
+            er = requests.get(shows())
+            D = []
+            pz = re.findall("index.*", er.text)
+            D.append(pz)
+            if gt_pix == "96":
+                print(X3 + D[0][0])
+            elif gt_pix == "144":
+                print(X3 + D[0][1])
+            elif gt_pix == "240":
+                print(X3 + D[0][2])
+            elif gt_pix == "360":
+                print(X3 + D[0][3])
+            elif gt_pix == "480":
+                print(X3 + D[0][4])
+            elif gt_pix == "570":
+                print(X3 + D[0][5])
+            elif gt_pix == "720":
+                print(X3 + D[0][6])
+            elif gt_pix == "1080":
+                print(X3 + D[0][7])
+            else:
+                print('check your quality option')
+        elif "master.m3u8" in shows():
+            X2 = (shows()[29:-11])
+            req_tk = requests.get("https://useraction.zee5.com/token/")
+            resp_tk = req_tk.content.decode('utf-8')
+            token = (eval(resp_tk)["video_token"])
+            get_pix = sys.argv[4]
+            X4 = (shows()[29:] + token)
+            Req = requests.get(X4)
+            G = []
+            Re = re.findall("index.*", Req.text)
+            G.append(Re)
+            if get_pix == "144":
+                print(X2 + G[0][0])
+            elif get_pix == "240":
+                print(X2 + G[0][1])
+            elif get_pix == "360":
+                print(X2 + G[0][2])
+            elif get_pix == "480":
+                print(X2 + G[0][3])
+            else:
+                print("check your quality ...")
+    except IndexError:
+        print("Usage : python3 shows.py {p}/{s} <link> -q <pixel>")
+    except TypeError:
+        print('\n Run help ( -h , --help) ')
+        
+if __name__ == "__main__":
+    main()
