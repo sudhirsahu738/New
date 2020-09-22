621d70b82a01d7140c5a19cf1a196fb3ad7a3f62
dedshit <62318734+dedshit@users.noreply.github.com>
2020-09-22 12:22:54 +0530

    stream 100%

---
+++ zee/headers.py
@@ -0,0 +1,11 @@
+headers = {
+    "User-Agent":"Mozilla/5.0 (Windows NT 6.1; rv:80.0) Gecko/20100101 Firefox/80.0",
+    "Referer":"https://www.zee5.com",
+    "Accept":"*/*",
+    "Accept-Encoding":"gzip, deflate, br",
+    "Accept-Language":"en-US,en;q=0.9",
+    "Origin":"https://www.zee5.com",
+    "sec-fetch-dest":"empty",
+    "sec-fetch-mode":"cors",
+    "sec-fetch-site":"same-site",
+}
