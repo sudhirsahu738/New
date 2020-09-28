d8b9e6146660c93f537cb96a2d772c0082a8a518
dedshit <62318734+dedshit@users.noreply.github.com>
2020-09-28 19:11:41 +0530

    Fix Token error
    now no more Token errors

--- zee/zee5.py
+++ zee/zee5.py
@@ -1,7 +1,7 @@
 import flask
 import re
 import requests
-import headers
+from headers import headers
 a = flask.Flask(__name__)
 @a.route('/')
 def home():
@@ -23,12 +23,16 @@ def contact():
 def con():
     return flask.render_template("v/ico.html")
 @a.route("/content/play")
-def api():    
+def api():   
     with open("_", 'r') as q1:
         try:
             w = q1.read()
             req1 = requests.get("https://useraction.zee5.com/tokennd").json()
+            rgx = re.findall("([0-9]?\w+)", w)[-3:]
             li = { "url":"zee5vodnd.akamaized.net", "token":"https://gwapi.zee5.com/content/details/" }
+            req2 = requests.get("https://useraction.zee5.com/token/platform_tokens.php?platform_name=web_app").json()["token"]
+            headers["X-Access-Token"] = req2
+            req3 = requests.get("https://useraction.zee5.com/token").json()
             htm = """
                 <!DOCTYPE html>
                 <html>
@@ -51,26 +55,33 @@ def api():
                 </html>
                 """                               
             if "movies" in w:
-                r = re.findall("0-0.*", w)[0]
-                r1 = requests.get(li["token"] + r, headers=headers.headers, params={"translation":"en", "country":"IN"}).json()
+                r1 = requests.get(li["token"] + "-".join(rgx),
+                                            headers=headers, 
+                                            params={"translation":"en", "country":"IN"}).json()
                 g1 = (r1["hls"][0].replace("drm", "hls") + req1["video_token"])
-                return htm.format(r1["title"], r1["image_url"], r1["title"], r1["age_rating"], r1["rating"], r1["duration"], r1["description"], "https://" + li["url"] + g1)
+                return htm.format(r1["title"], r1["image_url"], r1["title"], 
+                                    r1["age_rating"], r1["rating"], r1["duration"], 
+                                    r1["description"], "https://" + li["url"] + g1)
             elif "tvshows" or "originals" in w:
-                rr = re.findall("0-1.*", w)[0]
-                r2 = requests.get(li["token"] + rr, headers=headers.headers, params={"translation":"en", "country":"IN"}).json()
-                g2 = (r2["hls"][0].replace("drm", "hls") + req1["video_token"])
-                return htm.format(r2["title"], r2["image_url"], r2["title"], r2["age_rating"], r2["rating"], r2["duration"], r2["description"], "https://" + li["url"] + g2)
+                r2 = requests.get(li["token"] + "-".join(rgx), 
+                                            headers=headers, 
+                                            params={"translation":"en", "country":"IN"}).json()
+                g2 = (r2["hls"][0].replace("drm", "hls"))
+                if "netst" in g2:
+                    return htm.format(r2["title"], r2["image_url"], r2["title"], r2["age_rating"],
+                                      r2["rating"], r2["duration"], r2["description"], 
+                                      g2 + req3["video_token"])
+                else:
+                    return htm.format(r2["title"], r2["image_url"], r2["title"], r2["age_rating"], 
+                                            r2["rating"], r2["duration"], r2["description"], 
+                                            "https://" + li["url"] + g2 + req1["video_token"])
             else:
                 pass
-        except requests.exceptions.MissingSchema:
-            return { "message" : "Invalid url", "status" : "error" }, 206
         except IndexError:
             return flask.jsonify({ "message" : "No url specified" }), 200
         except requests.exceptions.ConnectionError:
-            return flask.jsonify({ "message" : "No connection" }), 200
-        except UnicodeDecodeError:
-            return { "message" : "can't decode site", "reason" : "unknown encrypted bytes", "status" : "error" }, 200
+            return flask.jsonify({ "message" : "No connection" })
         except KeyError:
-            return { "message" : "Try Again" }            
+            return { "message" : "Try Again" }, 200
 if __name__ == "__main__":
-    a.run("127.0.0.1", 8080, debug=False)
+    a.run("127.0.0.1", 8080, debug=True)
