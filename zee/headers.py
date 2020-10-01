349af20bb54171139d7d76147ecb3eea120851fb
dedshit <62318734+dedshit@users.noreply.github.com>
2020-10-01 11:47:42 +0530

    Update headers.py

--- zee/headers.py
+++ zee/headers.py
@@ -3,9 +3,10 @@ headers = {
     "Referer":"https://www.zee5.com",
     "Accept":"*/*",
     "Accept-Encoding":"gzip, deflate, br",
+    "Connection":"keep-alive",
     "Accept-Language":"en-US,en;q=0.9",
     "Origin":"https://www.zee5.com",
     "sec-fetch-dest":"empty",
     "sec-fetch-mode":"cors",
-    "sec-fetch-site":"same-site",
+    "sec-fetch-site":"same-site"
 }
